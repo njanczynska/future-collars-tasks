8dee154d573da31104a554f423b524b107cfb817
Natalia Janczynska <natalia.ako.j@gmail.com>
2022-04-08 19:10:36 +0200

    Final Movie file changes

--- src/main/java/Cinema/CinemaMain.java
+++
@@ -1,9 +0,0 @@
-package Cinema;
-
-public class CinemaMain {
-
-    public static void main(String[] args) {
-        Cinema cinema = new Cinema();
-        System.out.println(cinema.isStateFree());
-    }
-}
