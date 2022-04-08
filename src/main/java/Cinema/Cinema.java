6b5d70d0e1433339329f939e32850552adbe316e
Natalia Janczynska <natalia.ako.j@gmail.com>
2022-04-08 18:42:53 +0200

    Added Cinema class

---
+++ src/main/java/Cinema/Cinema.java
@@ -0,0 +1,13 @@
+package Cinema;
+
+public class Cinema {
+
+    private int numberOfSeat;
+    private int rowNumber;
+    private boolean stateIsFree;
+
+    public boolean isStateFree() {
+        return stateIsFree;
+    }
+
+}
