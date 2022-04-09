2aed70db869334345fef317def759e9bb67ff3bb
Natalia Janczynska <natalia.ako.j@gmail.com>
2022-04-09 22:40:27 +0200

    Revert "Final Movie file changes"
    This reverts commit 8dee154d573da31104a554f423b524b107cfb817.

---
+++ src/main/java/Cinema/CinemaMain.java
@@ -0,0 +1,9 @@
+package Cinema;
+
+public class CinemaMain {
+
+    public static void main(String[] args) {
+        Cinema cinema = new Cinema();
+        System.out.println(cinema.isStateFree());
+    }
+}
